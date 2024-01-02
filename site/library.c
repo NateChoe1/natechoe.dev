6c7f4c004fdeabea8eff8fe6a02bb67d2842227d
Nate Choe <nate@natechoe.dev>
2024-01-02 04:01:49 -0600

    Part of hashing, cursed favicon

--- site/library.c
+++ site/library.c
@@ -2,46 +2,57 @@
 #include <stdio.h>
 #include <string.h>
 
+#include <fcntl.h>
+#include <unistd.h>
+
 #include <swebs/util.h>
 #include <swebs/swebs.h>
 
-static long currday = -1;
-static char buff[300];
-static int currlen;
-
 static int getcolors(Request *request, Response *response);
+static int getfavicon(Request *request, Response *response);
 
 int getResponse(Request *request, Response *response) {
 	if (strcmp(request->path.path.data, "/autogen/colors.css") == 0)
 		return getcolors(request, response);
+	if (strcmp(request->path.path.data, "/favicon.ico") == 0)
+		return getfavicon(request, response);
 	response->type = DEFAULT;
 	return 404;
 }
 
-static int getcolors(Request *request, Response *response) {
-	long realday;
-
-	{
-		time_t currtime;
-		const time_t reference = 1655182800;
-		/* Midnight of the day of implementation in CST */
-		const int perday = 86400;
-		/* Seconds per day */
-		currtime = time(NULL);
-		if (currtime == -1)
-			realday = 0;
-		else
-			realday = (currtime - reference) / perday;
+static long getday() {
+	time_t currtime;
+	const time_t reference = 1655182800;
+	/* Midnight of the day of implementation in CST */
+	const int perday = 86400;
+	/* Seconds per day */
+	currtime = time(NULL);
+	if (currtime == -1) {
+		return 0;
 	}
+	return (currtime - reference) / perday;
+}
 
-	if (currday != realday) {
+static int getcolor(long day) {
+	/* The initial color at the time of implementation, just an arbitrary
+	 * choice that I liked */
+	const int initial = 203;
+	return (day + initial) % 360;
+}
+
+static int getcolors(Request *request, Response *response) {
+	static long lastcolorupdate = -1;
+	static char colorbuff[300];
+	static int colorlen = 0;
+
+	long today = getday();;
+
+	if (lastcolorupdate != today) {
 		int color;
-		const int initial = 203;
-		/* The initial color at the time of implementation */
-		color = (realday + initial) % 360;
-		currday = realday;
+		color = getcolor(today);
+		lastcolorupdate = today;
 
-		currlen = sprintf(buff,
+		colorlen = sprintf(colorbuff,
 ":root{"
 	"--backcol:hsl(%d,93%%,84%%);"
 	"--doccol:hsl(%d,92%%,75%%);"
@@ -50,11 +61,77 @@ static int getcolors(Request *request, Response *response) {
 	"--codecol:#000000;"
 	"--barcol:hsl(%d,96%%,68%%);"
 	"--textcol:#000000;"
-"} /*%ld %ld*/", color, color, color, currday, realday
+"} /*%ld %ld*/", color, color, color, lastcolorupdate, today
 		);
 	}
 	response->type = BUFFER_NOFREE;
-	response->response.buffer.data = buff;
-	response->response.buffer.len = currlen;
+	response->response.buffer.data = colorbuff;
+	response->response.buffer.len = colorlen;
+	return 200;
+}
+
+static int getfavicon(Request *request, Response *response) {
+	static long lastfaviconupdate = -1;
+
+	/* The important color is in BGRA format at this location in
+	 * /site/favicon.ico */
+	const int color_offset = 62;
+
+	/* The favicon is this many bytes */
+	const int favicon_len = 326;
+
+	int fd = open("/site/favicon.ico", O_RDWR);
+	if (fd == -1) {
+		request->type = DEFAULT;
+		return 500;
+	}
+
+	long today = getday();
+
+	if (lastfaviconupdate != today) {
+		int color;
+		unsigned char r, g, b;
+		color = getcolor(today);
+		switch (color / 60) {
+#define UP_CASE(offset, high, shift, low)  \
+		case offset: \
+			high = 255; \
+			low = 25; \
+			shift = 25 + (255-25)*(color-(offset)*60)/60;; \
+			break
+#define DOWN_CASE(offset, high, shift, low)  \
+		case offset: \
+			high = 255; \
+			low = 25; \
+			shift = 255 - (255-25)*(color-(offset)*60)/60;; \
+			break
+		UP_CASE(0, r, g, b);
+		DOWN_CASE(1, g, r, b);
+		UP_CASE(2, g, b, r);
+		DOWN_CASE(3, b, g, r);
+		UP_CASE(4, b, r, g);
+		DOWN_CASE(5, r, b, g);
+#undef UP_CASE
+#undef DOWN_CASE
+		default:
+			request->type = DEFAULT;
+			return 500;
+		}
+
+		unsigned char payload[] = {b, g, r};
+
+		lseek(fd, color_offset, SEEK_SET);
+		if (write(fd, payload, sizeof payload) < sizeof payload) {
+			close(fd);
+			request->type = DEFAULT;
+			return 500;
+		}
+		lastfaviconupdate = today;
+		lseek(fd, 0, SEEK_SET);
+	}
+
+	response->type = FILE_KNOWN_LENGTH;
+	response->response.file.fd = fd;
+	response->response.file.len = favicon_len;
 	return 200;
 }
