0d4091cead8daa5cc8073caa37b9046c59ab0fef
Nate Choe <nate@natechoe.dev>
2024-04-22 05:58:13 -0500

    88x31 buttons

--- site/library.c
+++ site/library.c
@@ -1,13 +1,5 @@
-/* It's been less than a week and my blog post is already inaccurate :(
- * 
- * https://natechoe.dev/blog/2023-12-28.html
- * 
- * I added a favicon to this page which also has to be dynamically generated so
- * I rewrote this entire file. The general control flow is still the same.
- *
- * By the way, I generate that favicon using an assembler! See site/favicon.asm
- * for more info.
- * */
+/* This website changes colors by 1 degree on the color wheel every day, which
+ * means that we have to dynamically generate a few resources. */
 
 #include <time.h>
 #include <stdio.h>
@@ -19,14 +11,32 @@
 #include <swebs/util.h>
 #include <swebs/swebs.h>
 
+/* When I first created natechoe.dev, the colors didn't change. I didn't even
+ * have support in swebs for dynamically generated responses. I just picked a
+ * nice looking cyan color and ran with it. Now that cyan color serves as a
+ * fallback whenever things go wrong. */
+#define INITIAL_COLOR 203
+
 static int getcolors(Request *request, Response *response);
-static int getfavicon(Request *request, Response *response);
+
+/* function name, file name, file length, BGR color offset, 255-saturation,
+ * brightness */
+#define BINARY_COLORS \
+	X(getfavicon, "/site/favicon.ico", 326, 62, 25, 255) \
+	X(getbutton, "/site/site/resources/88x31.bmp", 410, 30, 25, 255)
+
+#define X(funcname, filename, filelen, coloff, sat, bri) \
+	static int funcname(Request *request, Response *response);
+BINARY_COLORS
+#undef X
 
 int getResponse(Request *request, Response *response) {
 	if (strcmp(request->path.path.data, "/autogen/colors.css") == 0)
 		return getcolors(request, response);
 	if (strcmp(request->path.path.data, "/favicon.ico") == 0)
 		return getfavicon(request, response);
+	if (strcmp(request->path.path.data, "/resources/88x31.bmp") == 0)
+		return getbutton(request, response);
 	response->type = DEFAULT;
 	return 404;
 }
@@ -45,9 +55,7 @@ static long getday() {
 }
 
 static int getcolor(long day) {
-	/* The initial color at the time of implementation, just an arbitrary
-	 * choice that I liked */
-	const int initial = 203;
+	const int initial = INITIAL_COLOR;
 	return (day + initial) % 360;
 }
 
@@ -81,68 +89,65 @@ static int getcolors(Request *request, Response *response) {
 	return 200;
 }
 
-static int getfavicon(Request *request, Response *response) {
-	static long lastfaviconupdate = -1;
-
-	/* The important color is in BGRA format at this location in
-	 * /site/favicon.ico */
-	const int color_offset = 62;
-
-	/* The favicon is this many bytes */
-	const int favicon_len = 326;
-
-	int fd = open("/site/favicon.ico", O_RDWR);
-	if (fd == -1) {
-		request->type = DEFAULT;
-		return 500;
-	}
-
-	long today = getday();
-
-	if (lastfaviconupdate != today) {
-		int color;
-		unsigned char r, g, b;
-		color = getcolor(today);
-		switch (color / 60) {
+/* returns 0 on failure and 1 on success */
+static int injectcolor(int fd, int fileoffset, int hue, int satmin, int brightness) {
+	unsigned char r, g, b;
+	switch (hue / 60) {
 #define UP_CASE(offset, high, shift, low)  \
-		case offset: \
-			high = 255; \
-			low = 25; \
-			shift = 25 + (255-25)*(color-(offset)*60)/60;; \
-			break
+	case offset: \
+		high = brightness; \
+		low = satmin; \
+		shift = satmin + (brightness-satmin)*(hue-(offset)*60)/60;; \
+		break
 #define DOWN_CASE(offset, high, shift, low)  \
-		case offset: \
-			high = 255; \
-			low = 25; \
-			shift = 255 - (255-25)*(color-(offset)*60)/60;; \
-			break
-		UP_CASE(0, r, g, b);
-		DOWN_CASE(1, g, r, b);
-		UP_CASE(2, g, b, r);
-		DOWN_CASE(3, b, g, r);
-		UP_CASE(4, b, r, g);
-		DOWN_CASE(5, r, b, g);
+	case offset: \
+		high = brightness; \
+		low = satmin; \
+		shift = brightness - (brightness-satmin)*(hue-(offset)*60)/60;; \
+		break
+	UP_CASE(0, r, g, b);
+	DOWN_CASE(1, g, r, b);
+	UP_CASE(2, g, b, r);
+	DOWN_CASE(3, b, g, r);
+	UP_CASE(4, b, r, g);
+	DOWN_CASE(5, r, b, g);
 #undef UP_CASE
 #undef DOWN_CASE
-		default:
-			request->type = DEFAULT;
-			return 500;
-		}
-
-		unsigned char payload[] = {b, g, r};
-
-		lseek(fd, color_offset, SEEK_SET);
-		if (write(fd, payload, sizeof payload) < sizeof payload) {
-			close(fd);
-			request->type = DEFAULT;
-			return 500;
-		}
-		lastfaviconupdate = today;
-		lseek(fd, 0, SEEK_SET);
+	default:
+		return 0;
 	}
 
-	response->type = FILE_KNOWN_LENGTH;
-	response->response.file.fd = fd;
-	response->response.file.len = favicon_len;
-	return 200;
+	unsigned char payload[] = {b, g, r};
+
+	lseek(fd, fileoffset, SEEK_SET);
+	if (write(fd, payload, sizeof payload) < sizeof payload) {
+		return 0;
+	}
+	lseek(fd, 0, SEEK_SET);
+	return 1;
 }
+
+#define X(funcname, filename, filelen, coloff, sat, bri) \
+static int funcname(Request *request, Response *response) { \
+	static long BINARY_COLORS_LAST_UPDATE_##funcname = -1; \
+	int fd = open(filename, O_RDWR); \
+	if (fd == -1) { \
+		response->type = DEFAULT; \
+		return 500; \
+	} \
+\
+	long today = getday(); \
+	if (BINARY_COLORS_LAST_UPDATE_##funcname != today) { \
+		int color = getcolor(today); \
+		if (injectcolor(fd, coloff, color, sat, bri)) { \
+			BINARY_COLORS_LAST_UPDATE_##funcname = today; \
+		} \
+	} \
+\
+	response->type = FILE_KNOWN_LENGTH; \
+	response->response.file.fd = fd; \
+	response->response.file.len = filelen; \
+	return 200; \
+}
+BINARY_COLORS
+#undef X
