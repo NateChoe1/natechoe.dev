04ad461df42da4aaba96a9b2e5599156915d24f3
Nate Choe <nate@natechoe.dev>
2025-02-15 02:27:40 -0600

    blag

--- site/code.sh
+++ site/code.sh
@@ -9,8 +9,8 @@ fi
 TMPDIR="$(mktemp -d)"
 cat > "$TMPDIR/in"
 
-echo -n "<pre><code class=block>"
-vim -E -s -c 'syntax on' -c "set syntax=$1" -c 'runtime syntax/2html.vim' -c 'wqa' "$TMPDIR/in" > /dev/null 2>&1
+echo -n "<pre><code>"
+vim -E -s -c 'syntax on' -c "set ft=$1" -c 'runtime syntax/2html.vim' -c 'wqa' "$TMPDIR/in" > /dev/null 2>&1
 sed -e '0,/<pre/d' -e '/<\/pre>/,$d' -e 's/<span class="/<span class="highlight-/g' "$TMPDIR/in.html"
 echo -n "</code></pre>"
 #rm -r "$TMPDIR"
